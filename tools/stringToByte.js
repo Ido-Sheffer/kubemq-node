bc7b8bf558cdd9c4b45c3ce57acd18639aa7d955
Ido-Sheffer <ido.sheffer@kubemq.io>
2019-11-24 13:34:53 +0200

    fixed stringToByte

--- tools/stringToByte.js
+++ tools/stringToByte.js
@@ -23,9 +23,9 @@ SOFTWARE. */
 module.exports.stringToByte = function(body){
     let bytes                 =	  [];
 
-      for(let i = 0; i < "TestBody".length; i++) {
+      for(let i = 0; i < body.length; i++) {
 
-          let char              =   "TestBody".charCodeAt(i);
+          let char              =   body.charCodeAt(i);
           bytes.push(char >>> 8);
           bytes.push(char & 0xFF);
       }
